ea0f4e3834ec1eb02760a1d98916be0177c0d410
Kaloyan Karagyozov <karagyozovkaloyan@gmail.com>
2022-03-22 15:44:29 +0200

    regEx exercises

--- FUNDAMENTALS/31.Exercise Regular Expressions/01.furniture.js
+++ FUNDAMENTALS/31.Exercise Regular Expressions/01.furniture.js
@@ -1,20 +1,34 @@
 function solve(input) {
-
-
-    let pattern = />>([A-Za-z]+)<<(\d+(?:\.\d+)?)!(\d+)/;
-
-
-    while (input[0] != `Purchase`) {
-        let line = input.shift()
-        let match = pattern.exec(line);
-        if (match != `null`) {
-            let { name, price, qty } = match;
-        }
-
-
+  let pattern = />>(?<product>[A-Za-z]+)<<(?<price>\d+(?:\.\d+)?)!(?<qty>\d+)/;
+  let totalPrice = 0;
+  let products = [];
+  while (input[0] !== `Purchase`) {
+    let match = pattern.exec(input[0]);
+    if (match !== null) {
+      let name = match.groups.product;
+      products.push(name);
+      let price = Number(match.groups.price);
+      let quantity = Number(match.groups.qty);
+      totalPrice += price * quantity;
     }
-
+    input.shift();
+  }
+  if (products.length >= 1) {
+    console.log(`Bought furniture:`);
+    console.log(products.join(`\n`));
+    console.log(`Total money spend: ${totalPrice.toFixed(2)}`);
+  } else {
+    console.log("Bought furniture:");
+    console.log(`Total money spend: ${totalPrice.toFixed(2)}`);
+  }
 }
-solve(['>>Sofa<<312.23!3', '>>TV<<300!5', '>Invalid<<!5', 'Purchase']
-
-)   
+solve([
+  ">>Laptop<<312.2323!3",
+  ">>TV<<300.21314!5",
+  ">Invalid<<!5",
+  ">>TV<<300.21314!20",
+  ">>Invalid<!5",
+  ">>TV<<30.21314!5",
+  ">>Invalid<<!!5",
+  "Purchase",
+]);
---
+++ FUNDAMENTALS/31.Exercise Regular Expressions/dasdad.js
@@ -0,0 +1,31 @@
+function demo(furnitures) {
+
+    let pattern = />>(?<type>[A-Za-z]+)<<(?<price>[\d\.]+)!(?<quantity>\d+)/g;
+    let output = [];
+    let sum = 0;
+
+    while ((result = pattern.exec(furnitures)) !== null) {
+
+        output.push(result.groups.type);
+        let quantity = Number(result.groups.quantity);
+        let price = Number(result.groups.price);
+        let finalSum = quantity * price;
+        sum += finalSum;
+
+    }
+    if (output.length > 0) {
+        console.log('Bought furniture:');
+        console.log(output.join('\n'));
+        console.log(`Total money spend: ${sum.toFixed(2)}`);
+    } else {
+        console.log('Bought furniture:');
+        console.log(`Total money spend: ${sum.toFixed(2)}`);
+    }
+
+
+
+}
+demo(['>Invalid<<!4',
+    '>Invalid<<!2',
+    '>Invalid<<!5',
+    'Purchase'])
