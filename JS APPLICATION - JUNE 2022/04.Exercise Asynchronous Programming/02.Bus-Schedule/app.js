69aed4167c9416fccfd340a520acbfc2fd8bc990
Kaloyan Karagyozov <karagyozovkaloyan@gmail.com>
2022-07-05 22:17:18 +0300

    application exer

--- JS APPLICATION - JUNE 2022/04.Exercise Asynchronous Programming/02.Bus-Schedule/app.js
+++ JS APPLICATION - JUNE 2022/04.Exercise Asynchronous Programming/02.Bus-Schedule/app.js
@@ -1,17 +1,36 @@
 function solve() {
+  let departBtn = document.getElementById(`depart`);
+  let arriveBtn = document.getElementById(`arrive`);
+  let spaneElement = document.querySelector(`#info span`);
+  let busStop = {
+    next: `depot`,
+  };
+  function depart() {
+    let url = `http://localhost:3030/jsonstore/bus/schedule/${busStop.next}`;
 
-    function depart() {
-        console.log('Depart TODO...');
-    }
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        busStop = data;
+        spaneElement.textContent = `Next stop ${busStop.name}`;
+      })
+      .catch((error) => (spaneElement.textContent = `Error`));
 
-    function arrive() {
-        console.log('Arrive TODO...');
-    }
+    departBtn.disabled = true;
+    arriveBtn.disabled = false;
+  }
 
-    return {
-        depart,
-        arrive
-    };
+  function arrive() {
+    spaneElement.textContent = `Arriving at ${busStop.name}`;
+
+    departBtn.disabled = false;
+    arriveBtn.disabled = true;
+  }
+
+  return {
+    depart,
+    arrive,
+  };
 }
 
 let result = solve();
