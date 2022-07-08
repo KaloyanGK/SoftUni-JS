58f88a5354ea130419822700c7c465e2486b9639
Kaloyan Karagyozov <karagyozovkaloyan@gmail.com>
2022-07-08 21:50:31 +0300

    async exer

--- JS APPLICATION - JUNE 2022/04.Exercise Asynchronous Programming/03.Forecaster/app.js
+++ JS APPLICATION - JUNE 2022/04.Exercise Asynchronous Programming/03.Forecaster/app.js
@@ -1,94 +1,131 @@
 function attachEvents() {
-  //ВЗИМАМЕ ИНПУТ ПОЛЕТО+БУТОНА
-  let inputElement = document.getElementById(`location`);
-  let button = document.getElementById(`submit`);
-  let currentDiv = document.getElementById(`current`);
-  let upcomingDiv = document.getElementById(`upcoming`);
-  let forecastDiv = document.getElementById(`forecast`);
-  let url = `http://localhost:3030/jsonstore/forecaster`;
+    //ВЗИМАМЕ ИНПУТ ПОЛЕТО+БУТОНА
+    let inputElement = document.getElementById(`location`);
+    let button = document.getElementById(`submit`);
+    let currentDiv = document.getElementById(`current`);
+    let upcomingDiv = document.getElementById(`upcoming`);
+    let forecastDiv = document.getElementById(`forecast`);
+    let url = `http://localhost:3030/jsonstore/forecaster`;
 
-  let divForecastCurrent = document.createElement(`div`);
-  let divForecastUpcoming = document.createElement(`div`);
+    let divForecastCurrent = document.createElement(`div`);
+    let divForecastUpcoming = document.createElement(`div`);
 
-  let sunny = `☀`;
-  let partlySunny = `⛅`;
-  let overcast = `☁`;
-  let rain = `☂`;
-  let degrees = `°`;
+    let sunny = `☀`;
+    let partlySunny = `⛅`;
+    let overcast = `☁`;
+    let rain = `☂`;
+    let degrees = `°`;
 
-  let code = ``;
+    let code = ``;
 
-  button.addEventListener(`click`, (e) => {
-    divForecastUpcoming.innerHTML = "";
-    divForecastCurrent.innerHTML = "";
+    button.addEventListener(`click`, (e) => {
+        divForecastUpcoming.innerHTML = "";
+        divForecastCurrent.innerHTML = "";
 
-    divForecastUpcoming.setAttribute(`class`, `forecast-info`);
-    divForecastCurrent.setAttribute(`class`, `forecasts`);
+        divForecastUpcoming.setAttribute(`class`, `forecast-info`);
+        divForecastCurrent.setAttribute(`class`, `forecasts`);
 
-    forecastDiv.style.display = `inline`;
+        forecastDiv.style.display = `inline`;
 
-    fetch(`${url}/locations`)
-      .then((responce) => responce.json())
-      .then((data) => {
-        data.forEach((el) => {
-          if (el.name == inputElement.value) {
-            code = el.code;
-          }
-        });
+        fetch(`${url}/locations`)
+            .then((responce) => responce.json())
+            .then((data) => {
+                data.forEach((el) => {
+                    if (el.name == inputElement.value) {
+                        code = el.code;
+                    }
+                });
 
-        fetch(`${url}/today/${code}`)
-          .then((responce) => responce.json())
-          .then((data) => {
-            let condition = data.forecast.condition;
-            let cityName = data.name;
-            let lowTemp = data.forecast.low;
-            let highTmp = data.forecast.high;
+                fetch(`${url}/today/${code}`)
+                    .then((responce) => responce.json())
+                    .then((data) => {
+                        let condition = data.forecast.condition;
+                        let cityName = data.name;
+                        let lowTemp = data.forecast.low;
+                        let highTmp = data.forecast.high;
 
-            let conditionSymbolSpan = document.createElement(`span`);
-            conditionSymbolSpan.setAttribute(`class`, `condition symbol`);
+                        let conditionSymbolSpan = document.createElement(`span`);
+                        conditionSymbolSpan.setAttribute(`class`, `condition symbol`);
 
-            if (condition == `Sunny`) {
-              conditionSymbolSpan.textContent = sunny;
-            } else if (condition == `Overcast`) {
-              conditionSymbolSpan.textContent = overcast;
-            } else if (condition == `Partly sunny`) {
-              conditionSymbolSpan.textContent = partlySunny;
-            } else if (condition == `Rain`) {
-              conditionSymbolSpan.textContent = rain;
-            }
-            let conditionSpan = document.createElement(`span`);
-            conditionSpan.setAttribute(`class`, `condition`);
+                        if (condition == `Sunny`) {
+                            conditionSymbolSpan.textContent = sunny;
+                        } else if (condition == `Overcast`) {
+                            conditionSymbolSpan.textContent = overcast;
+                        } else if (condition == `Partly sunny`) {
+                            conditionSymbolSpan.textContent = partlySunny;
+                        } else if (condition == `Rain`) {
+                            conditionSymbolSpan.textContent = rain;
+                        }
+                        let conditionSpan = document.createElement(`span`);
+                        conditionSpan.setAttribute(`class`, `condition`);
 
-            let cityNameSpan = document.createElement(`span`);
-            cityNameSpan.setAttribute(`class`, `forecast-data`);
-            cityNameSpan.textContent = cityName;
+                        let cityNameSpan = document.createElement(`span`);
+                        cityNameSpan.setAttribute(`class`, `forecast-data`);
+                        cityNameSpan.textContent = cityName;
 
-            let temperatureSpan = document.createElement(`span`);
-            temperatureSpan.setAttribute(`class`, `forecast-data`);
-            temperatureSpan.textContent = `${lowTemp}${degrees}/${highTmp}${degrees}`;
+                        let temperatureSpan = document.createElement(`span`);
+                        temperatureSpan.setAttribute(`class`, `forecast-data`);
+                        temperatureSpan.textContent = `${lowTemp}${degrees}/${highTmp}${degrees}`;
 
-            let weatherSpan = document.createElement(`span`);
-            weatherSpan.setAttribute(`class`, `forecast-data`);
-            weatherSpan.textContent = condition;
+                        let weatherSpan = document.createElement(`span`);
+                        weatherSpan.setAttribute(`class`, `forecast-data`);
+                        weatherSpan.textContent = condition;
 
-            conditionSpan.appendChild(cityNameSpan);
-            conditionSpan.appendChild(temperatureSpan);
-            conditionSpan.appendChild(weatherSpan);
+                        conditionSpan.appendChild(cityNameSpan);
+                        conditionSpan.appendChild(temperatureSpan);
+                        conditionSpan.appendChild(weatherSpan);
 
-            divForecastCurrent.appendChild(conditionSymbolSpan);
-            divForecastCurrent.appendChild(conditionSpan);
+                        divForecastCurrent.appendChild(conditionSymbolSpan);
+                        divForecastCurrent.appendChild(conditionSpan);
 
-            currentDiv.appendChild(divForecastCurrent);
-          })
-          .catch((error) => (inputElement.value = `Error`));
+                        currentDiv.appendChild(divForecastCurrent);
+                    })
+                    .catch((error) => (inputElement.value = `Error`));
 
-        fetch(`${url}/upcoming/${code}`)
-          .then((responce) => responce.json())
-          .then((data) => {
-            console.log(data);
-          });
-      })
-  });
+                fetch(`${url}/upcoming/${code}`)
+                    .then((responce) => responce.json())
+                    .then((data) => {
+                        data.forecast.forEach(el => {
+
+                            let condition = el.condition;
+                            let lowTemp = el.low;
+                            let highTmp = el.high;
+
+                            let conditionSymbolSpan = document.createElement(`span`);
+                            conditionSymbolSpan.setAttribute(`class`, `condition symbol`);
+
+                            if (condition == `Sunny`) {
+                                conditionSymbolSpan.textContent = sunny;
+                            } else if (condition == `Overcast`) {
+                                conditionSymbolSpan.textContent = overcast;
+                            } else if (condition == `Partly sunny`) {
+                                conditionSymbolSpan.textContent = partlySunny;
+                            } else if (condition == `Rain`) {
+                                conditionSymbolSpan.textContent = rain;
+                            }
+                            let conditionSpan = document.createElement(`span`);
+                            conditionSpan.setAttribute(`class`, `upcoming`);
+
+
+                            let temperatureSpan = document.createElement(`span`);
+                            temperatureSpan.setAttribute(`class`, `forecast-data`);
+                            temperatureSpan.textContent = `${lowTemp}${degrees}/${highTmp}${degrees}`;
+
+                            let weatherSpan = document.createElement(`span`);
+                            weatherSpan.setAttribute(`class`, `forecast-data`);
+                            weatherSpan.textContent = condition;
+
+                            conditionSpan.appendChild(conditionSymbolSpan);
+                            conditionSpan.appendChild(temperatureSpan);
+                            conditionSpan.appendChild(weatherSpan);
+                            divForecastUpcoming.appendChild(conditionSpan);
+                            upcomingDiv.appendChild(divForecastUpcoming)
+                        })
+
+                    })
+                    .catch((error) => (inputElement.value = `Error`));
+            })
+    });
 }
 
 attachEvents();
