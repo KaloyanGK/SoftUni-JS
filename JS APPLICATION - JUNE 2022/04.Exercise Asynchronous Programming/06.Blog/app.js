60e55ce0da0eb1a3abc9c765e2507fba7a1638d4
Kaloyan Karagyozov <karagyozovkaloyan@gmail.com>
2022-07-11 23:45:27 +0300

    exer asynch

--- JS APPLICATION - JUNE 2022/04.Exercise Asynchronous Programming/06.Blog/app.js
+++ JS APPLICATION - JUNE 2022/04.Exercise Asynchronous Programming/06.Blog/app.js
@@ -1,26 +1,56 @@
 function attachEvents() {
-  let loadPostsBtn = document.getElementById(`btnLoadPosts`);
+
+  let postUrl = `http://localhost:3030/jsonstore/blog/posts`;
+  let commentsUrl = `http://localhost:3030/jsonstore/blog/comments`;
+
+  let loadBtn = document.getElementById(`btnLoadPosts`);
   let selectElement = document.getElementById(`posts`);
   let viewBtn = document.getElementById(`btnViewPost`);
 
-  let postsUrl = `http://localhost:3030/jsonstore/blog/posts`;
-  let commentsUrl = `http://localhost:3030/jsonstore/blog/comments`;
+  let h1 = document.getElementById('post-title');
+  let p = document.getElementById('post-body');
+  let ul = document.getElementById('post-comments');
 
-  loadPostsBtn.addEventListener(`click`, (e) => {
-    fetch(postsUrl)
-      .then((res) => res.json())
-      .then((data) => {
+  let posts = [];
+
+  loadBtn.addEventListener(`click`, (e) => {
+    fetch(postUrl)
+      .then(res => res.json())
+      .then(data => {
         let info = Object.values(data);
-        info.forEach((el) => {
-          let title = el.title;
-          let id = el.id;
+        info.forEach(el => {
           let optionElement = document.createElement(`option`);
-          optionElement.textContent = title;
-          optionElement.value = id;
+          optionElement.setAttribute(`value`, el.id);
+          optionElement.textContent = el.title;
+          posts.push(el);
           selectElement.appendChild(optionElement);
-        });
-      });
-  });
+        })
+      })
+    viewBtn.addEventListener(`click`, (e) => {
+
+      fetch(commentsUrl)
+        .then(res => res.json())
+        .then(data => {
+          let value = selectElement.value;
+          let currentPost = posts.find(x => x.id == value);
+          h1.textContent = currentPost.title;
+          p.textContent = currentPost.body;
+          Object.values(data).forEach(el => {
+            if (el.postId == value) {
+                let li = document.createElement('li');
+                li.id = value;
+                li.textContent = el.text;
+                ul.appendChild(li);
+            }
+        })
+        })
+    })
+  })
+
+
 }
 
 attachEvents();
+
+
+
