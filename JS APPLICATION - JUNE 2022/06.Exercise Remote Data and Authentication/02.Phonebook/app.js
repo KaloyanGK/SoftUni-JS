2ad2c134a92e6684c7da059ca5906a6dfaf9be60
Kaloyan Karagyozov <karagyozovkaloyan@gmail.com>
2022-07-15 18:56:38 +0300

    exercise Async

--- JS APPLICATION - JUNE 2022/06.Exercise Remote Data and Authentication/02.Phonebook/app.js
+++ JS APPLICATION - JUNE 2022/06.Exercise Remote Data and Authentication/02.Phonebook/app.js
@@ -1,5 +1,76 @@
 function attachEvents() {
-    console.log('TODO...');
+
+    const url = `http://localhost:3030/jsonstore/phonebook`;
+    const loadBtn = document.querySelector(`#btnLoad`);
+    const createBtn = document.querySelector(`#btnCreate`);
+    const ul = document.querySelector(`#phonebook`);
+
+    loadBtn.addEventListener(`click`, (e) => {
+        ul.replaceChildren()
+        fetch(url)
+            .then(res => res.json())
+            .then(data => {
+
+                let personDetails = Object.values(data);
+                personDetails.forEach(p => {
+                    let li = document.createElement(`li`);
+                    let deleteBtn = document.createElement(`button`);
+
+                    deleteBtn.textContent = `Delete`;
+                    li.textContent = `${p.person}: ${p.phone}`;
+                    li.appendChild(deleteBtn)
+                    ul.appendChild(li)
+
+
+                    deleteBtn.addEventListener(`click`, (e) => {
+                        let object = {
+                            [p._id]: {
+                                person: p.person,
+                                phone: p.phone,
+                            }
+
+                        }
+                        let key = Object.keys(object).join();
+                        console.log(key);
+                        fetch(`http://localhost:3030/jsonstore/phonebook/${key}`, {
+                            method: 'DELETE',
+                            headers: {
+                                'content-type': 'application/json'
+                            },
+                            body: JSON.stringify(object)
+                        })
+                            .then(res => res.json())
+                            .catch(error => alert(error))
+                    })
+                })
+            })
+
+    })
+    createBtn.addEventListener(`click`, (e) => {
+        let personElement = document.querySelector(`#person`);
+        let phoneElement = document.querySelector(`#phone`);
+        let person = personElement.value;
+        let phone = phoneElement.value;
+        if (!person || !phone) {
+            return
+        }
+        let object = {
+            person,
+            phone,
+        }
+        fetch(url, {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(object)
+        })
+            .then(res => res.json())
+            .catch(error => alert(error))
+        personElement.value = ``;
+        phoneElement.value = ``;
+
+    })
 }
 
 attachEvents();
--- JS APPLICATION - JUNE 2022/06.Exercise Remote Data and Authentication/03.Students/app.js
+++ JS APPLICATION - JUNE 2022/06.Exercise Remote Data and Authentication/03.Students/app.js
@@ -1,1 +1,78 @@
-console.log('TODO...');
+function solve() {
+    const button = document.getElementById(`submit`);
+    const url = `http://localhost:3030/jsonstore/collections/students`;
+    const tableBody = document.querySelector(`#results tbody`)
+
+
+    fetch(url)
+        .then(res => res.json())
+        .then(data => {
+
+            let studentInfo = Object.values(data);
+            studentInfo.forEach(student => {
+                let firstName = student.firstName;
+                let lastName = student.lastName;
+                let facultyNumber = student.facultyNumber;
+                let grade = student.grade;
+
+                let tr = document.createElement(`tr`);
+
+                let tdFirstName = document.createElement(`td`);
+                tdFirstName.textContent = firstName;
+
+                let tdlastName = document.createElement(`td`);
+                tdlastName.textContent = lastName;
+
+                let tdfacultyNumber = document.createElement(`td`);
+                tdfacultyNumber.textContent = facultyNumber;
+
+                let tdGrade = document.createElement(`td`);
+                tdGrade.textContent = grade;
+
+                tr.appendChild(tdFirstName)
+                tr.appendChild(tdlastName)
+                tr.appendChild(tdfacultyNumber)
+                tr.appendChild(tdGrade)
+                tableBody.appendChild(tr)
+
+
+            })
+        })
+    button.addEventListener(`click`, (e) => {
+        e.preventDefault()
+        const firstNameInput = document.querySelector(`[name='firstName']`);
+        const lastNameInput = document.querySelector(`[name='lastName']`);
+        const facultyNumberInput = document.querySelector(`[name='facultyNumber']`);
+        const gradeInput = document.querySelector(`[name='grade']`);
+        let firstName = firstNameInput.value;
+        let lastName = lastNameInput.value;
+        let facultyNumber = facultyNumberInput.value;
+        let grade = gradeInput.value;
+        if (!firstName || !lastName || !facultyNumber || !grade) {
+            return
+        }
+        let object = {
+            firstName,
+            lastName,
+            facultyNumber,
+            grade,
+        }
+        fetch(url, {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(object)
+        })
+            .then(res => res.json())
+            .catch(error => alert(error))
+        firstNameInput.value = ``;
+        lastNameInput.value = ``;
+        facultyNumberInput.value = ``;
+        gradeInput.value = ``;
+    })
+
+
+
+
+}
